92c2e4f2417674a36e1e68c4174b5a683953f5fd
Diogo-Gomes <37155647+Diogo-Gomes@users.noreply.github.com>
2019-12-04 18:22:01 -0300

    Update heliusV7.c

--- heliusV7.c
+++ heliusV7.c
@@ -46,18 +46,12 @@ int main(void)
         //while(i==0){
         for(int cont=1;cont<=7;cont++)
         {
-                //puts("Digite um numero entre 1 e 5");
-                //puts("para mandar o MSP430 piscar seus LEDs,");
-                //puts("ou digite 0 para terminar o programa. ");
-                //scanf("%d", &user_input);
-		user_input = cont;
-                //printf("Pedindo valor do sensor %d\n",user_input);
-
+                user_input = cont;
+                
                 write(i2c_fd, &user_input, 1);
                 sleep(0.5);
                 read(i2c_fd, &msp430_ret,1);
                 sleep(0.5);
-                //printf("msp retornou = %d\n \n", msp430_ret);
 
                 if (cont==1){
                         a5=msp430_ret*(1023/255);
@@ -92,24 +86,13 @@ int main(void)
 			rot=(msp430_ret*(1023/255)*100*resoluc);
 			printf("Rotação do Stirling = %.2f\n", rot);
 		}
-                //else if((user_input<0) || (user_input>5))
-                //      puts("Valor invalido");
-                //else if(user_input>0)
-                //{
-                //      write(i2c_fd, &user_input, 1);
-                //      sleep(1+user_input/2);
-                //      read(i2c_fd, &msp430_ret, 1);
-                //      printf("MSP430_return = %d\n", msp430_ret);
-                //}
+                
                 puts("");
-                //user_input++;
+		
         }
-        //}
+        
         close(i2c_fd);
 	char user_input2=1, msp430_ret2, slave_addr2=0x0A;
-        //unsigned int c=0;
-        //double ad5,ad4,ad3,ad2,ad1,ad0;
-        //double media_temp, divt1, divt2, corgerad, corbat, pot_bat, pot_ger;
 
         signal(SIGINT, ctrl_c);
         i2c_fd = open("/dev/i2c-1", O_RDWR);
@@ -118,26 +101,22 @@ int main(void)
         for(int cont2=1;cont2<=6;cont2++)
         {
                 user_input2 = cont2;
-                //printf("Pedindo valor do sensor %d\n",user_input2);
-
-                write(i2c_fd, &user_input2, 1);
+		write(i2c_fd, &user_input2, 1);
                 sleep(0.5);
                 read(i2c_fd, &msp430_ret2,1);
                 sleep(0.5);
-                //printf("msp retornou = %d\n \n", msp430_ret2);
+                
                 if (cont2==1){
                         ad5=msp430_ret2*(1023/255);
 			ldr_500=ad5*resoluc;
                         printf("Valor do sensor em AD5 (LDR_2) é: %.2f\n", ad5);
 			printf("Valor do ldr_500: %.2f\n", ldr_500);
-			//printf("krad*ldr_500 = %.2f\n", krad*ldr_500);
                 }
                 else if (cont2==2){
                         ad4=msp430_ret2*(1023/255);
 			ldr_1k=ad4*resoluc;
                         printf("Valor do sensor em AD4 (LDR_3) é: %.2f\n", ad4);
 			printf("Valor do ldr_1k: %.2f\n", ldr_1k);
-			//printf("krad*ldr_1k = %.2f\n", krad*ldr_1k);
                 }
                 else if (cont2==3){
                         ad3=msp430_ret2*(1023/255);
@@ -154,7 +133,6 @@ int main(void)
                 else if (cont2==5){
                         ad1=msp430_ret2*(1023/255); 
                         corgerad=((((ad1*resoluc)-2.5)*1000)/185);
-			//corgerad=((((ad1*resoluc.)+1.725)*1000)/185);
                         printf("Valor do sensor em AD1 (CORRENTE_GERADOR): %.2f\n",ad1);
 			printf("Valor de CORRENTE_GERADOR: %.2f\n",corgerad);
                 }
@@ -172,23 +150,18 @@ int main(void)
 
 	if(1.93<ldr_1k<3.33){
 		G = (47.5*(ldr_1k*ldr_1k)) - (4.4*(100)*ldr_1k) + 1048;
-		//G=(47.5*(ldr_1k*ldr_1k))−(4.4*(10e2)*ldr_1k)+1048;
 	}
 	else if(0.266<ldr_100<0.427){
 		G = (5.5*(1000)*(ldr_100*ldr_100)) - (6.3*1000*ldr_100)+(2*1000);
-		//G=(5.5*(10^3))*(ldr_100^2)−(6.3*(10^3)*ldr_100)+(2*(10^3));
 	}
 	else if(0.224<ldr_100<0.273){
 		G  = (1.93*10000*ldr_100*ldr_100)-(1.7*10000*ldr_100)+(4000);
-		//G=(1.93*(10^4)*(ldr_100^2))−(1.7*(10^4)*ldr_100)+(4*(10^3));
 	}
 	else if(0.203<ldr_100<0.231){
 		G = (4.5*10000*ldr_100*ldr_100)-(3.3*10000*ldr_100)+(6.500);
-		//G=((4.5*(10^4))*(ldr_100^2))−(3.3*(10^4)*ldr_100)+(6.5*(10^3));
 	}
 	else if(0.259<ldr_500<0.294){
 		G = (4.8*10000*ldr_500*ldr_500)-(4.2*10000*ldr_500)+9999.7; 
-		//G=(4.8*(10^4)*(ldr_500^2))-(4.2*(10^4)*ldr_500)+9999.7;
 	}
 	else {
 		G=0;
@@ -197,24 +170,6 @@ int main(void)
 	G=1000-G;
 	printf("Irradiação estimada: %.2f\n", G);
 	
-	/*
-	if(2.75<ldr_1K<4.76 ){
-		G = (47.5*(ldr_1k^2)) − (4.4*(10^2)*ldr_1k) + 1048;
-	}
-	else if (0.38<ldr_100<0.61){
-		G = (5.5*(10^3))*(ldr_100^2) − (6.3*(10^3)*ldr_100) + (2*(10^3));
-	}
-	else if (0.32<ldr_100<0.39){
-		G = (1.93*(10^4)*(ldr_100^2)) − (1.7*(10^4)*ldr_100) + (4*(10^3));
-	}
-	else if (0.29<ldr_100<0.33){
-		G = (4.5*(10^4))*(ldr_100^2) − (3.3*(10^4)*ldr_100) + (6.5*(10^3));
-	}
-	else if (0.37<ldr_100<0.42){
-	G = (4.8*(10^4)*(ldr_100^2)) - (4.2*(10^4)*ldr_100) + 9999.7;
-	}
-	*/
-
 	if(uv_sensor<0.05){
 		uv_index=0;
 		uv_irad=uv_index/40;
@@ -276,8 +231,7 @@ int main(void)
 	media_temp =(a1+a2)/2;
 	FILE * fp; //int x[5] = {0,1,2,5,5};
 	fp = fopen ("file.csv", "w+");
-        //char c[] = "oi";
-        //fputc(x,fp);
+        
         fprintf(fp, "TEMP_REFLETOR,TEMP_PFRIO,IRRAD_LDR,IRRAD_UV,VBAT,VGER,IBAT,IGER,ROT,TEMP_PQUENTE,GPS_LAT,GPS_LONG\n");
         fprintf(fp, "%f,%f,%f,%f,%f,%f,%f,%f,%f,",media_temp,a0,G,uv_irad,divt1,divt2,corbat,corgerad,rot);
         fclose(fp);
@@ -294,18 +248,8 @@ int main(void)
 	char comanddate[50];
 	char aspas = '"';
 	
-	//int c_date=0;
-	//strcpy(aspas);
 	if(i_date==0){	
 		system("sudo python3 horario.py");
-		//system("sudo echo${VARIABLE}");
-		//system("sudo date -s ${VARIABLE}");
-	//	strcpy(comanddate,"sudo date -s ");
-	//	strcat(comanddate,&aspas);
-	//	strcat(comanddate,"Sun Dec 01 12:34:11 UTC-03 2019");
-	//	strcat(comanddate,&aspas);
-		//system(sudo date -s "Sat Nov 29 12:05:11 UTC-03 2019");
-	//	system(comanddate);
 		i_date = 1;
 	}
 
@@ -313,7 +257,7 @@ int main(void)
 	strcpy(comand3, "python3 script.py");
 	system(comand3);
 
-	//sleep(5);
+	sleep(5);
 
 	}
         return (0);
