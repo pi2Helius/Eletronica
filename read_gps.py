6eb65460d98da9fa5dca1b0db247a8c8ced9d964
Diogo-Gomes <37155647+Diogo-Gomes@users.noreply.github.com>
2019-12-04 18:43:57 -0300

    Update read_gps.py

--- read_gps.py
+++ read_gps.py
@@ -3,6 +3,7 @@ import time
 import serial
 import sys
 
+#Configurando comunicação serial
 ser = serial.Serial(
         port='/dev/serial0',
         baudrate = 9600,
@@ -16,9 +17,8 @@ i=0
 
 while (i==0):
 
-        line=str(ser.readline())
-        #print (line)
-        #for line in x:
+        line=str(ser.readline())        #Lê linha por linha do arquivo onde os dados são escritos
+        # Serarando os dados recebidos de modo a escrever a localização em termos de latitude e longitude
         if line.startswith("b'$GPRMC"):
                 line = line.strip()
                 #print (line)
@@ -27,16 +27,12 @@ while (i==0):
                 graus_lat = latitude[0]+latitude[1]
                 longitude = dados[5]
                 graus_long = longitude[0]+longitude[1]+longitude[2]
-                #coordenadas = graus_lat+'°'+latitude[2:]+"'"+ dados[4]+','+graus_long+'°'+longitude[3:]+"'"+dados[6]
                 coordenadas = graus_lat+latitude[2:]+ dados[4]+","+graus_long+longitude[3:]+dados[6]
                 print(coordenadas)
                 print(graus_lat+latitude[2:]+ dados[4]+",")
                 print(graus_long+longitude[3:]+dados[6])
 
-
-                #graus_lat = "068.5743N"
-                #graus_long = "123.213W"
-
+                #Escrevendo as coordenadas no arquivo
                 arq = open("file.csv","a")
                 arq.write(graus_lat+latitude[2:]+ dados[4]+",")
                 arq.write(graus_long+longitude[3:]+dados[6])
